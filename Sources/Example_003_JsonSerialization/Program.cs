2d85bf3745cc37a63b4cabeeb0dfa71361a55674
Anton Ivanchenko <iv.anton98@gmail.com>
2022-02-08 00:58:35 +0200

    Add json serialization/deserialization example

---
+++ Sources/Example_003_JsonSerialization/Program.cs
@@ -0,0 +1,20 @@
+﻿using Example_003_JsonSerialization;
+using Sharp.ValueObject.Json;
+using System.Text.Json;
+
+var options = new JsonSerializerOptions()
+{
+    Converters = { new ValueObjectConverterFactory() }
+};
+
+var temperatureRange = new TemperatureRange()
+{
+    Min = new Temperature(-5.0F),
+    Max = Temperature.WaterEvaporation
+};
+
+string temperatureRangeJson = JsonSerializer.Serialize(temperatureRange, options);
+Console.WriteLine($"Temperature range: {temperatureRangeJson}");
+
+TemperatureRange range = JsonSerializer.Deserialize<TemperatureRange>(temperatureRangeJson, options)!;
+Console.WriteLine($"Temperature range from {range.Min} to {range.Max}");
---
+++ Sources/Example_003_JsonSerialization/TemperatureRange.cs
@@ -0,0 +1,8 @@
+﻿namespace Example_003_JsonSerialization
+{
+    public class TemperatureRange
+    {
+        public Temperature? Min { get; set; }
+        public Temperature? Max { get; set; }
+    }
+}
