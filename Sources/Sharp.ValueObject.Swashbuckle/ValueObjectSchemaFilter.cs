b4303a536eb1e66c8b10a461c4a21fa789902620
Anton Ivanchenko <iv.anton98@gmail.com>
2022-02-09 10:51:37 +0200

    Add Sharp.ValueObject.Swashbuckle project

---
+++ Sources/Sharp.ValueObject.Swashbuckle/ValueObjectSchemaFilter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Sharp.ValueObject.Swashbuckle
+{
+    public class ValueObjectSchemaFilter : ISchemaFilter
+    {
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+        {
+            if (!ValueObject.IsValueObjectType(context.Type))
+                return;
+
+            var valueProperties = schema.Properties
+                .Where(c => string.Equals(c.Key, "value", StringComparison.OrdinalIgnoreCase))
+                .Select(c => c.Value)
+                .ToList();
+
+            if (valueProperties.Count == 1)
+            {
+                var valueProperty = valueProperties[0];
+
+                schema.Type = valueProperty.Type;
+                valueProperty.Properties.Clear();
+            }
+        }
+    }
+}
