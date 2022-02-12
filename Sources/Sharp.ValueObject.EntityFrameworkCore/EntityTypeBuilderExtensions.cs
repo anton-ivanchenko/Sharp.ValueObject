9041a90a0094287b30360d408a8b701f0b967174
Anton Ivanchenko <iv.anton98@gmail.com>
2022-02-12 17:30:24 +0200

    Add Sharp.ValueObject.EntityFrameworkCore project

---
+++ Sources/Sharp.ValueObject.EntityFrameworkCore/EntityTypeBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Linq.Expressions;
+
+namespace Sharp.ValueObject.EntityFrameworkCore
+{
+    public static class EntityTypeBuilderExtensions
+    {
+        public static ValueObjectBuilder<TEntity, TValueObject> ValueObject<TEntity, TValueObject>(this EntityTypeBuilder<TEntity> builder, Expression<Func<TEntity, TValueObject?>> propertyExpression)
+            where TEntity : class
+            where TValueObject : ValueObject
+        {
+            return new ValueObjectBuilder<TEntity, TValueObject>(propertyExpression, builder, builder.OwnsOne(propertyExpression));
+        }
+    }
+}
---
+++ Sources/Sharp.ValueObject.EntityFrameworkCore/ValueObjectBuilder.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Linq.Expressions;
+
+namespace Sharp.ValueObject.EntityFrameworkCore
+{
+    public class ValueObjectBuilder<TEntity, TValueObject>
+        where TEntity : class
+        where TValueObject : ValueObject
+    {
+        private readonly EntityTypeBuilder<TEntity> _entityTypeBuilder;
+        private readonly OwnedNavigationBuilder<TEntity, TValueObject> _ownedTypeBuilder;
+
+        private readonly Expression<Func<TEntity, TValueObject?>> _valueObjectProperty;
+
+        public ValueObjectBuilder(
+            Expression<Func<TEntity, TValueObject?>> valueObject,
+            EntityTypeBuilder<TEntity> entityTypeBuilder,
+            OwnedNavigationBuilder<TEntity, TValueObject> ownedTypeBuilder)
+        {
+            _ownedTypeBuilder = ownedTypeBuilder;
+            _valueObjectProperty = valueObject;
+            _entityTypeBuilder = entityTypeBuilder;
+        }
+
+        public ValueObjectBuilder<TEntity, TValueObject> ValueProperty(Action<PropertyBuilder> action)
+        {
+            action.Invoke(_ownedTypeBuilder.Property("Value"));
+            return this;
+        }
+
+        public ValueObjectBuilder<TEntity, TValueObject> IsRequired(bool required = true)
+        {
+            _entityTypeBuilder.Navigation(_valueObjectProperty).IsRequired(required);
+            return this;
+        }
+    }
+}
